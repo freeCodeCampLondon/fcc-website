9b6dd68cba303f48b95295db12f60ab906655b40
alexanderbrown <alexanderbrown.info@gmail.com>
2024-10-23 14:35:40 +0100

    add codewars challenge data

---
+++ src/data/codewarsChallenges.ts
@@ -0,0 +1,586 @@
+export type CodeWarsChallenge = {
+  title: string;
+  link: string;
+}
+export const codeWarsChallenges: CodeWarsChallenge[] = [
+  {
+    title: "Returning Strings",
+    link: "https://www.codewars.com/kata/55a70521798b14d4750000a4"
+  },
+  {
+    title: "Find numbers which are divisible by given number",
+    link: "https://www.codewars.com/kata/55edaba99da3a9c84000003b"
+  },
+  {
+    title: "Rock Paper Scissors!",
+    link: "https://www.codewars.com/kata/5672a98bdbdd995fad00000f"
+  },
+  {
+    title: "Stringy Strings",
+    link: "https://www.codewars.com/kata/563b74ddd19a3ad462000054"
+  },
+  {
+    title: "Credit Card Mask",
+    link: "https://www.codewars.com/kata/5412509bd436bd33920011bc"
+  },
+  {
+    title: "Number of People in the Bus",
+    link: "https://www.codewars.com/kata/5648b12ce68d9daa6b000099"
+  },
+  {
+    title: "Get the mean of an array",
+    link: "https://www.codewars.com/kata/563e320cee5dddcf77000158"
+  },
+  {
+    title: "How many are smaller than me?",
+    link: "https://www.codewars.com/kata/56a1c074f87bc2201200002e"
+  },
+  {
+    title: "Enumerable Magic #1 - True for All?",
+    link: "https://www.codewars.com/kata/54598d1fcbae2ae05200112c/javascript"
+  },
+  {
+    title: "Prefill an Array",
+    link: "https://www.codewars.com/kata/54129112fb7c188740000162"
+  },
+  {
+    title: "Generate range of integers",
+    link: "https://www.codewars.com/kata/55eca815d0d20962e1000106"
+  },
+  {
+    title: "A Needle in the Haystack",
+    link: "https://www.codewars.com/kata/56676e8fabd2d1ff3000000c"
+  },
+  {
+    title: "Opposites Attract",
+    link: "https://www.codewars.com/kata/555086d53eac039a2a000083/javascript"
+  },
+  {
+    title: "Count by X",
+    link: "https://www.codewars.com/kata/5513795bd3fafb56c200049e"
+  },
+  {
+    title: "Between Extremes",
+    link: "https://www.codewars.com/kata/56d19b2ac05aed1a20000430"
+  },
+  {
+    title: "Spoonerize Me",
+    link: "https://www.codewars.com/kata/56b8903933dbe5831e000c76"
+  },
+  {
+    title: "Find Count of Most Frequent Item in an Array",
+    link: "https://www.codewars.com/kata/56582133c932d8239900002e"
+  },
+  {
+    title: "Sexy Primes <3",
+    link: "https://www.codewars.com/kata/56b58d11e3a3a7cade000792"
+  },
+  {
+    title: "Basic method",
+    link: "https://www.codewars.com/kata/55da49c166949c319200003e"
+  },
+  {
+    title: "Be Concise III - Sum Squares",
+    link: "https://www.codewars.com/kata/56f8fe6a2e6c0dc83b0008a7"
+  },
+  {
+    title: "Decoding a message",
+    link: "https://www.codewars.com/kata/565b9d6f8139573819000056"
+  },
+  {
+    title: "DNA GC-content",
+    link: "https://www.codewars.com/kata/5747a9bbe2fab9a0c400012f"
+  },
+  {
+    title: "Recursive Replication",
+    link: "https://www.codewars.com/kata/57547f9182655569ab0008c4"
+  },
+  {
+    title: "Xmas Tree",
+    link: "https://www.codewars.com/kata/577c349edf78c178a1000108"
+  },
+  {
+    title: "Test Your Knowledge Of Function Scope",
+    link: "https://www.codewars.com/kata/56d344c7fd3a52566700124b"
+  },
+  {
+    title: "Make acronym",
+    link: "https://www.codewars.com/kata/57a60bad72292d3e93000a5a"
+  },
+  {
+    title: "Hells Kitchen",
+    link: "https://www.codewars.com/kata/57d1f36705c186d018000813"
+  },
+  {
+    title: "The dropWhile Function",
+    link: "https://www.codewars.com/kata/54f9c37106098647f400080a/javascript"
+  },
+  {
+    title: "Duck Shoot - Easy Version",
+    link: "https://www.codewars.com/kata/57d27a0a26427672b900046f/javascript"
+  },
+  {
+    title: "Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe",
+    link: "https://www.codewars.com/kata/582746fa14b3892727000c4f"
+  },
+  {
+    title: "Reverse Factorials",
+    link: "https://www.codewars.com/kata/58067088c27998b119000451/javascript"
+  },
+  {
+    title: "Is every value in the array an array?",
+    link: "https://www.codewars.com/kata/582c81d982a0a65424000201"
+  },
+  {
+    title: "Spacify",
+    link: "https://www.codewars.com/kata/57f8ee485cae443c4d000127"
+  },
+  {
+    title: "Naughty or Nice?",
+    link: "https://www.codewars.com/kata/585eaef9851516fcae00004d"
+  },
+  {
+    title: "Merge two arrays",
+    link: "https://www.codewars.com/kata/583af10620dda4da270000c5"
+  },
+  {
+    title: "Currying functions: multiply all elements in an array",
+    link: "https://www.codewars.com/kata/586909e4c66d18dd1800009b"
+  },
+  {
+    title: "String transformer",
+    link: "https://www.codewars.com/kata/5878520d52628a092f0002d0"
+  },
+  {
+    title: "Reversing Words in a String",
+    link: "https://www.codewars.com/kata/57a55c8b72292d057b000594"
+  },
+  {
+    title: "Sum of a sequence",
+    link: "https://www.codewars.com/kata/586f6741c66d18c22800010a"
+  },
+  {
+    title: "Grains",
+    link: "https://www.codewars.com/kata/55f7eb009e6614447b000099"
+  },
+  {
+    title: "Thinkful - List Drills: Longest word",
+    link: "https://www.codewars.com/kata/58670300f04e7449290000e5"
+  },
+  {
+    title: "max diff - easy",
+    link: "https://www.codewars.com/kata/588a3c3ef0fbc9c8e1000095"
+  },
+  {
+    title: "Make a square box!",
+    link: "https://www.codewars.com/kata/58644e8ddf95f81a38001d8d"
+  },
+  {
+    title: "The Office II - Boredom Score",
+    link: "https://www.codewars.com/kata/57ed4cef7b45ef8774000014"
+  },
+  {
+    title: "Remove String Spaces",
+    link: "https://www.codewars.com/kata/57eae20f5500ad98e50002c5"
+  },
+  {
+    title: "The Office IV - Find a Meeting Room",
+    link: "https://www.codewars.com/kata/57f604a21bd4fe771b00009c"
+  },
+  {
+    title: "Most digits",
+    link: "https://www.codewars.com/kata/58daa7617332e59593000006"
+  },
+  {
+    title: "Adding words - Part I",
+    link: "https://www.codewars.com/kata/592eaf848c91f248ca000012"
+  },
+  {
+    title: "Duck Duck Goose",
+    link: "https://www.codewars.com/kata/582e0e592029ea10530009ce"
+  },
+  {
+    title: "How many animals are there?",
+    link: "https://www.codewars.com/kata/593406b8f3d071d83c00005d"
+  },
+  {
+    title: "8kyu interpreters: HQ9+",
+    link: "https://www.codewars.com/kata/591588d49f4056e13f000001"
+  },
+  {
+    title: "Friday the 13th Part 1",
+    link: "https://www.codewars.com/kata/5925acf31a9825d616000e74"
+  },
+  {
+    title: "isEven? - Bitwise Series",
+    link: "https://www.codewars.com/kata/592a33e549fe9840a8000ba1"
+  },
+  {
+    title: "Odd or Even?",
+    link: "https://www.codewars.com/kata/5949481f86420f59480000e7"
+  },
+  {
+    title: "Digital cypher",
+    link: "https://www.codewars.com/kata/592e830e043b99888600002d"
+  },
+  {
+    title: "Bubblesort Once",
+    link: "https://www.codewars.com/kata/56b97b776ffcea598a0006f2"
+  },
+  {
+    title: "Number Zoo Patrol",
+    link: "https://www.codewars.com/kata/5276c18121e20900c0000235"
+  },
+  {
+    title: "English beggars",
+    link: "https://www.codewars.com/kata/59590976838112bfea0000fa"
+  },
+  {
+    title: "Categorize New Member",
+    link: "https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa"
+  },
+  {
+    title: "Contamination #1 -String-",
+    link: "https://www.codewars.com/kata/596fba44963025c878000039"
+  },
+  {
+    title: "Only one",
+    link: "https://www.codewars.com/kata/5734c38da41454b7f700106e"
+  },
+  {
+    title: "Classy Classes",
+    link: "https://www.codewars.com/kata/55a144eff5124e546400005a"
+  },
+  {
+    title: "Factorial Factory",
+    link: "https://www.codewars.com/kata/528e95af53dcdb40b5000171/javascript"
+  },
+  {
+    title: "Closest to Zero",
+    link: "https://www.codewars.com/kata/59887207635904314100007b"
+  },
+  {
+    title: "esreveR",
+    link: "https://www.codewars.com/kata/5413759479ba273f8100003d"
+  },
+  {
+    title: "Implement Array.prototype.filter()",
+    link: ""
+  },
+  {
+    title: "Sum Times Tables",
+    link: "https://www.codewars.com/kata/551e51155ed5ab41450006e1"
+  },
+  {
+    title: "Recursive Ninja",
+    link: "https://www.codewars.com/kata/583ed487ee36e2a8d20000e8"
+  },
+  {
+    title: "By 3, or not by 3? That is the question . . .",
+    link: "https://www.codewars.com/kata/59f7fc109f0e86d705000043"
+  },
+  {
+    title: "Array plus array",
+    link: "https://www.codewars.com/kata/5a2be17aee1aaefe2a000151"
+  },
+  {
+    title: "All unique",
+    link: "https://www.codewars.com/kata/553e8b195b853c6db4000048"
+  },
+  {
+    title: "Even numbers in an array",
+    link: "https://www.codewars.com/kata/5a431c0de1ce0ec33a00000c"
+  },
+  {
+    title: "Possiblities Array",
+    link: "https://www.codewars.com/kata/59b710ed70a3b7dd8f000027"
+  },
+  {
+    title: "Man in the west",
+    link: "https://www.codewars.com/kata/59bd5dc270a3b7350c00008b"
+  },
+  {
+    title: "Matrix creation",
+    link: "https://www.codewars.com/kata/5a34da5dee1aae516d00004a"
+  },
+  {
+    title: "Custom Christmas Tree",
+    link: "https://www.codewars.com/kata/5a405ba4e1ce0e1d7800012e"
+  },
+  {
+    title: "Remove B M W",
+    link: "https://www.codewars.com/kata/59de795c289ef9197f000c48"
+  },
+  {
+    title: "Alphabet symmetry",
+    link: "https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0"
+  },
+  {
+    title: "Reversed Strings",
+    link: "https://www.codewars.com/kata/5168bb5dfe9a00b126000018"
+  },
+  {
+    title: "Are the numbers in order?",
+    link: "https://www.codewars.com/kata/56b7f2f3f18876033f000307"
+  },
+  {
+    title: "Word values",
+    link: "https://www.codewars.com/kata/598d91785d4ce3ec4f000018"
+  },
+  {
+    title: "Convert Color image to greyscale",
+    link: "https://www.codewars.com/kata/590ee3c979ae8923bf00095b"
+  },
+  {
+    title: "Maximum Gap (Array Series #4)",
+    link: "Array Series #4"
+  },
+  {
+    title: "Tidy Number (Special Numbers Series #9)",
+    link: "Special Numbers Series #9"
+  },
+  {
+    title: "Row Weights",
+    link: "https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9"
+  },
+  {
+    title: "The Deca Tree",
+    link: "https://www.codewars.com/kata/5acf710f46b4cb00810001e2/javascript"
+  },
+  {
+    title: "Building Strings From a Hash",
+    link: "https://www.codewars.com/kata/51c7d8268a35b6b8b40002f2"
+  },
+  {
+    title: "Equal Sides Of An Array",
+    link: "https://www.codewars.com/kata/5679aa472b8f57fb8c000047"
+  },
+  {
+    title: "Product Array (Array Series #5)",
+    link: "Array Series #5"
+  },
+  {
+    title: "Dominant array elements",
+    link: "https://www.codewars.com/kata/5a04133e32b8b998dc000089/javascript"
+  },
+  {
+    title: "If you can't sleep, just count sheep!!",
+    link: "https://www.codewars.com/kata/5b077ebdaf15be5c7f000077"
+  },
+  {
+    title: "Offload your work!",
+    link: "https://www.codewars.com/kata/5b3e1dca3da310a4390000f3"
+  },
+  {
+    title: "Help the Fruit Guy",
+    link: "https://www.codewars.com/kata/557af4c6169ac832300000ba"
+  },
+  {
+    title: "altERnaTIng cAsE <=> ALTerNAtiNG CaSe",
+    link: "https://www.codewars.com/kata/56efc695740d30f963000557"
+  },
+  {
+    title: "Every possible sum of two digits",
+    link: "https://www.codewars.com/kata/5b4e474305f04bea11000148"
+  },
+  {
+    title: "Find heavy ball - level: novice",
+    link: "https://www.codewars.com/kata/544047f0cf362503e000036e"
+  },
+  {
+    title: "Drone Fly-By",
+    link: "https://www.codewars.com/kata/58356a94f8358058f30004b5"
+  },
+  {
+    title: "Who's Online?",
+    link: "https://www.codewars.com/kata/5b6375f707a2664ada00002a"
+  },
+  {
+    title: "Remove All The Marked Elements of a List",
+    link: "https://www.codewars.com/kata/563089b9b7be03472d00002b"
+  },
+  {
+    title: "I guess this is a 7kyu kata #6: Fruit Ninja I",
+    link: "https://www.codewars.com/kata/57d60363a65454701d000e11"
+  },
+  {
+    title: "Even odd disparity",
+    link: "https://www.codewars.com/kata/59c62f1bdcc40560a2000060/javascript"
+  },
+  {
+    title: "Quicksum",
+    link: "https://www.codewars.com/kata/569924899aa8541eb200003f"
+  },
+  {
+    title: "Rap-Name Generator",
+    link: "https://www.codewars.com/kata/5c06a930717256064b0001ab"
+  },
+  {
+    title: "Coloured Triangles",
+    link: "https://www.codewars.com/kata/5a25ac6ac5e284cfbe000111"
+  },
+  {
+    title: "Odds-Index",
+    link: "https://www.codewars.com/kata/5a941f4e1a60f6e8a70025fe"
+  },
+  {
+    title: "Duplicate Encoder",
+    link: "https://www.codewars.com/kata/54b42f9314d9229fd6000d9c"
+  },
+  {
+    title: "Rot13",
+    link: "https://www.codewars.com/kata/530e15517bc88ac656000716"
+  },
+  {
+    title: "Squares sequence",
+    link: "https://www.codewars.com/kata/5546180ca783b6d2d5000062"
+  },
+  {
+    title: "My Languages",
+    link: "https://www.codewars.com/kata/5b16490986b6d336c900007d"
+  },
+  {
+    title: "Figure Out the Notes",
+    link: "https://www.codewars.com/kata/5602e85d255e3240c2000024"
+  },
+  {
+    title: "Bingo ( Or Not )",
+    link: " Or Not "
+  },
+  {
+    title: "Sum of numbers from 0 to N",
+    link: "https://www.codewars.com/kata/56e9e4f516bcaa8d4f001763"
+  },
+  {
+    title: "Split an array",
+    link: "https://www.codewars.com/kata/550aea365951383698000727"
+  },
+  {
+    title: "String Scramble",
+    link: "https://www.codewars.com/kata/5822d89270ca28c85c0000f3"
+  },
+  {
+    title: "Sub-array elements sum",
+    link: "https://www.codewars.com/kata/5b5e0ef007a26632c400002a"
+  },
+  {
+    title: "Complementary DNA",
+    link: "https://www.codewars.com/kata/554e4a2f232cdd87d9000038"
+  },
+  {
+    title: "How many arguments",
+    link: "https://www.codewars.com/kata/5c44b0b200ce187106452139"
+  },
+  {
+    title: "Friend or Foe?",
+    link: "https://www.codewars.com/kata/55b42574ff091733d900002f"
+  },
+  {
+    title: "Is it a palindrome?",
+    link: "https://www.codewars.com/kata/57a1fd2ce298a731b20006a4"
+  },
+  {
+    title: "Computer problem series #1: Fill the Hard Disk Drive",
+    link: "https://www.codewars.com/kata/5d49c93d089c6e000ff8428c"
+  },
+  {
+    title: "Eliminate the intruders! Bit manipulation",
+    link: "https://www.codewars.com/kata/5a0d38c9697598b67a000041"
+  },
+  {
+    title: "Reverse words",
+    link: "https://www.codewars.com/kata/5259b20d6021e9e14c0010d4"
+  },
+  {
+    title: "Sum of Array Averages",
+    link: "https://www.codewars.com/kata/56d5166ec87df55dbe000063"
+  },
+  {
+    title: "Find the Missing Number",
+    link: "https://www.codewars.com/kata/57f5e7bd60d0a0cfd900032d"
+  },
+  {
+    title: "Highest Rank Number in an Array",
+    link: "https://www.codewars.com/kata/5420fc9bb5b2c7fd57000004"
+  },
+  {
+    title: "Odd Ones Out!",
+    link: "https://www.codewars.com/kata/5d376cdc9bcee7001fcb84c0"
+  },
+  {
+    title: "Valid Parentheses",
+    link: "https://www.codewars.com/kata/52774a314c2333f0a7000688"
+  },
+  {
+    title: "Finding length of the sequence",
+    link: "https://www.codewars.com/kata/5566b0dd450172dfc4000005"
+  },
+  {
+    title: "Count letters in string",
+    link: "https://www.codewars.com/kata/5808ff71c7cfa1c6aa00006d"
+  },
+  {
+    title: "The First Non Repeated Character In A String",
+    link: "https://www.codewars.com/kata/570f6436b29c708a32000826"
+  },
+  {
+    title: "Band name generator",
+    link: "https://www.codewars.com/kata/59727ff285281a44e3000011"
+  },
+  {
+    title: "Counting Valleys",
+    link: "https://www.codewars.com/kata/5da9973d06119a000e604cb6"
+  },
+  {
+    title: "Count the divisible numbers",
+    link: "https://www.codewars.com/kata/55a5c82cd8e9baa49000004c"
+  },
+  {
+    title: "Most valuable character",
+    link: "https://www.codewars.com/kata/5dd5128f16eced000e4c42ba"
+  },
+  {
+    title: "Summing a number's digits",
+    link: "https://www.codewars.com/kata/52f3149496de55aded000410"
+  },
+  {
+    title: "Sum of Minimums!",
+    link: "https://www.codewars.com/kata/5d5ee4c35162d9001af7d699"
+  },
+  {
+    title: "Search for letters",
+    link: "https://www.codewars.com/kata/52dbae61ca039685460001ae"
+  },
+  {
+    title: "Mirror, mirror, on the wall...",
+    link: "https://www.codewars.com/kata/5f55ecd770692e001484af7d"
+  },
+  {
+    title: "Fizz Buzz",
+    link: "https://www.codewars.com/kata/5300901726d12b80e8000498"
+  },
+  {
+    title: "Middle Me",
+    link: "https://www.codewars.com/kata/59cd155d1a68b70f8e000117"
+  },
+  {
+    title: "Double Trouble",
+    link: "https://www.codewars.com/kata/57f7796697d62fc93d0001b8"
+  },
+  {
+    title: "Elimination Tournament",
+    link: "https://www.codewars.com/kata/5f631ed489e0e101a70c70a0"
+  },
+  {
+    title: "Streaky Patterns in Coin Flips",
+    link: "https://www.codewars.com/kata/5c1ac4f002c59c725900003f"
+  },
+  {
+    title: "Gravity Flip",
+    link: "https://www.codewars.com/kata/5f70c883e10f9e0001c89673"
+  },
+  {
+    title: "Filter Coffee",
+    link: "https://www.codewars.com/kata/56069d0c4af7f633910000d3/solutions"
+  },
+]
